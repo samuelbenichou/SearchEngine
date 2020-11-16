818f4750c810ba9903b5b0755b8d0e1727b4b6dd
samuelbenichou <samuelbenichou@hotmail.fr>
2020-11-17 01:55:44 +0200

    add multi threading for the indexer

--- search_engine.py
+++ search_engine.py
@@ -4,6 +4,7 @@ from parser_module import Parse
 from indexer import Indexer
 from searcher import Searcher
 import utils
+import concurrent.futures
 
 
 def run_engine():
@@ -22,10 +23,14 @@ def run_engine():
     documents_list = r.read_file(file_name='~/Desktop/Corpus')
     print("Number of tweets in the corpus {}".format(len(documents_list)))
 
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.submit(parse_index_doc, documents_list)
+
+
     #print(documents_list)
 
     # Iterate over every document in the file
-    for idx, document in enumerate(documents_list):
+    """for idx, document in enumerate(documents_list):
         # parse the document
         parsed_document = p.parse_doc(document)
         number_of_documents += 1
@@ -38,8 +43,31 @@ def run_engine():
     print('Finished parsing and indexing. Starting to export files')
 
     utils.save_obj(indexer.inverted_idx, "inverted_idx")
-    utils.save_obj(indexer.postingDict, "posting")
+    utils.save_obj(indexer.postingDict, "posting")"""
 
+def parse_index_doc(documents_list):
+    config = ConfigClass()
+    p = Parse()
+    indexer = Indexer(config)
+    print("Number of tweets in the corpus {}".format(len(documents_list)))
+
+    # print(documents_list)
+
+    # Iterate over every document in the file
+    for idx, document in enumerate(documents_list):
+        # parse the document
+        parsed_document = p.parse_doc(document)
+
+        # index the document data
+        indexer.add_new_doc(parsed_document)
+    # print(indexer.inverted_idx)
+    # print(indexer.postingDict)
+    print(indexer.get_tweet_info('1280966306292998145'))
+    print(indexer.get_term_info("Donald Trump"))
+    print('Finished parsing and indexing. Starting to export files')
+
+    utils.save_obj(indexer.inverted_idx, "inverted_idx")
+    utils.save_obj(indexer.postingDict, "posting")
 
 def load_index():
     print('Load inverted index')
